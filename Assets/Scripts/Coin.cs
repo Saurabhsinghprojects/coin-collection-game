3450835b12469947395a72fa45a1448069b1a5b6
Saurabh singh <gamingfreakunity@gmail.com>
2022-07-02 17:43:39 +0530

    Initial commit

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public static int CoinCount = 0;
+    //A static variable is declared with the help of static keyword. When a variable is declared as static, then a single copy of the variable is created and shared among all objects at the class level. Static variables are accessed with the name of the class, they do not require any object for access.
+
+    void Start()
+    {
+        CoinCount++;
+
+    }
+    void OnTriggerEnter(Collider Col)
+    {
+        //if player collided then destroy the coin see current object is coin so hm destroy(gameobject) likh skte hain bcz no need to specify the coin as it is game object itself 
+         if (Col.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        CoinCount--;
+        if (CoinCount <= 0)
+        {
+            //We won 
+            GameObject Timer = GameObject.Find("LevelTimer");
+            Destroy(Timer);
+            GameObject[] Fireworks = GameObject.FindGameObjectsWithTag("Fireworks");
+            foreach(GameObject Go in Fireworks)
+            {
+                Go.GetComponent<ParticleSystem>().Play(); // notice that playonAwake = enabled work nhi kr raha hai
+            }
+
+        
+        }
+
+    } 
+    void Update()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //for using sceneManager
+public class Timer : MonoBehaviour
+{
+    public float MaxTime = 60f;
+    [SerializeField]
+    private float CountDown = 0f;
+
+    void Start()
+    {
+        CountDown = MaxTime;       
+    }
+
+   
+    void Update()
+    {
+        CountDown -= Time.deltaTime;
+        //here we have used time.deltaTime in update bcz suppose your game has 2fps then your deltaTime will be o.5 hence in 1sec update will invoke 2 times as 2fps so total time substracted from countdown is 2*0.5 =1sec
+
+        if (CountDown<= 0f)
+        {
+            Coin.CoinCount = 0;
+            SceneManager.LoadScene(0);// this is the new way of loading scene application.Loadlevel is Obsolete now
+        }
+
+    }
+}
